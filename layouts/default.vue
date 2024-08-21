9b6508d9a562ff0453b12ce4ff32d10a53036cd7
Vrijraj Singh <vrijraj2396@gmail.com>
2024-08-21 20:56:22 +0530

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,36 +1,8 @@
 <template>
-  <v-app style="background-color: white" class="">
-    <v-app-bar
-      :elevation="0"
-      class="mx-auto mt-4 px-3"
-      rounded="xl"
-      color="#eeeeee"
-      style="
-        max-width: 1024px;
-        margin-left: auto;
-        margin-right: auto;
-        position: relative;
-      "
-    >
-      <!-- <template v-slot:prepend>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      </template> -->
-      <v-app-bar-title
-        ><NuxtLink to="/" style="text-decoration: none; color: black">
-          DevFest City
-        </NuxtLink></v-app-bar-title
-      >
+  <v-app style="background-color: white" class="google-font">
+    <CoreAppToolbar />
+    <CoreAppDrawer/>
 
-      <!-- <v-app-bar-title>
-        <NuxtLink to="/">DevFest City</NuxtLink>
-      </v-app-bar-title> -->
-
-      <v-btn rounded size="small" to="/speakers" class="mx-1">Speakers</v-btn>
-      <v-btn rounded size="small" to="/agenda" class="mx-1">Agenda</v-btn>
-      <v-btn rounded size="small" to="/badge" class="mx-1">Badge</v-btn>
-      <v-btn rounded size="small" to="/team" class="mx-1">Team</v-btn>
-      <v-btn rounded size="small" to="/faq" class="mx-1">FAQ</v-btn>
-    </v-app-bar>
     <v-main
       class="pa-0"
       style="max-width: 1400px; margin-left: auto; margin-right: auto"
@@ -40,21 +12,66 @@
 
     <v-container
       fluid
-      class="pa-0 mt-8"
+      class="px-md-0 px-5 mt-8"
       style="max-width: 1400px; margin-left: auto; margin-right: auto"
     >
       <v-divider class="mb-4"></v-divider>
-      <v-row>
-        <v-col md="2">
-          <h3>GDG XYZ</h3>
+      <v-row justify="center" align="center">
+        <v-col md="2" cols="12">
+          <h3>{{ configDataSet.communityName }}</h3>
           <p><i>developed by OSS Labs</i></p>
         </v-col>
-        <v-col md="10">
+        <v-col md="10" cols="12">
           <v-container fluid class="pa-0">
-            <v-row>
-              <v-col md="3" v-for="i in 4" :key="i">
-                <NuxtLink style="display: block;" v-for="i in 4" :key="i">Link 1</NuxtLink>
+            <v-row >
+              <v-col>
+                <a
+                  href="https://developers.google.com/community/gdg"
+                  target="_blank"
+                  rel="noreferrer"
+                  class="grey--text text--darken-3 mr-6 google-font"
+                  style="text-decoration: none;color:black"
+                  >About GDG Program</a
+                >
+                <br class="hidden-md-and-up" />
+                <a
+                  href="https://www.womentechmakers.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  class="grey--text text--darken-3 mr-6 google-font"
+                  style="text-decoration: none;color:black"
+                  >About WTM Program</a
+                >
+
+                <br class="hidden-md-and-up" />
+                <router-link
+                  to="/faq"
+                  rel="noreferrer"
+                  class="mr-3 google-font grey--text text--darken-3"
+                  style="text-decoration: none;color:black"
+                  >FAQ</router-link
+                >
+                <br class="hidden-md-and-up" />
+                <router-link
+                  to="/coc"
+                  style="text-decoration: none;color:black"
+                  rel="noreferrer"
+                  class="mr-3 google-font grey--text text--darken-3"
+                  >Code of conduct</router-link
+                >
+                <br class="hidden-md-and-up" />
+                <a
+                  href="https://developers.google.com/community-guidelines"
+                  target="_blank"
+                  rel="noreferrer"
+                  class="mr-6 google-font grey--text text--darken-3"
+                  style="text-decoration: none;color:black"
+                  >Community Guidelines</a
+                >
               </v-col>
+              <!-- <v-col md="3" v-for="i in 4" :key="i">
+                <NuxtLink style="display: block;" v-for="i in 4" :key="i">Link 1</NuxtLink>
+              </v-col> -->
             </v-row>
           </v-container>
         </v-col>
@@ -62,3 +79,9 @@
     </v-container>
   </v-app>
 </template>
+
+<script setup>
+import configData from "/public/data/config.json";
+const configDataSet = ref([]);
+configDataSet.value = configData;
+</script>

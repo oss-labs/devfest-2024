f3769944084594e5ad7887309cb33f57c557c54f
Vrijraj Singh <vrijraj2396@gmail.com>
2024-08-21 20:55:38 +0530

    Update pastDevFest.vue

--- components/home/pastDevFest.vue
+++ components/home/pastDevFest.vue
@@ -8,30 +8,23 @@
           class="text-white"
         >
           <v-row class="pa-0 ma-0" justify="center" align="center">
-            <v-col md="8" cols="12" class="pa-10">
+            <v-col md="8" cols="12" class="pa-md-10">
               <h1>Last Year at DevFest</h1>
               <p class="mb-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Sapiente amet asperiores unde. Adipisci nobis ipsum fuga amet?
-                Ut, iusto tenetur quisquam neque consectetur impedit magnam unde
-                harum a mollitia possimus? Lorem ipsum, dolor sit amet
-                consectetur adipisicing elit. Asperiores laborum ut praesentium
-                architecto a. Recusandae neque deserunt saepe mollitia quibusdam
-                a nihil dicta in, totam hic molestias eaque. Dolores, nisi.
+                {{ data.summery }}
               </p>
 
-              <v-chip v-for="i in 4" :key="i" class="mr-1 mb-1"
-                >20 Speakers</v-chip
+              <v-chip v-for="(item,index) in data.stats" :key="index" class="mr-1 mb-1"
+                >{{ item.value }} {{item.name}}</v-chip
               >
             </v-col>
             <v-col md="4" class="pa-0">
               <v-img
                 gradient="to right, rgba(28,29,29,1), rgba(0,0,0,.1)"
-                src="https://picsum.photos/500/300"
+                src="/img/common/past-devfest.png"
                 rounded="lg"
                 class="justify-end"
               >
-                <!-- <v-chip>DevFest 2023</v-chip> -->
               </v-img>
             </v-col>
           </v-row>
@@ -42,7 +35,9 @@
 </template>
 
 <script>
-export default {};
+export default {
+  props:['data']
+};
 </script>
 
 <style>

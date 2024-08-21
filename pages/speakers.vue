deda4918086cd8bd795e4999fade94f6f84714f0
vrijraj <vrijraj2396@gmail.com>
2024-08-21 14:54:40 +0530

    speaker loader added

--- pages/speakers.vue
+++ pages/speakers.vue
@@ -1,20 +1,32 @@
 <template>
-   <NuxtLayout name="default">
-  <v-container fluid>
-    <v-row>
+  <NuxtLayout name="default">
+    <v-container fluid>
+      <v-row>
         <v-col md="12">
-            <h1>Speakers</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, sapiente est architecto autem minus explicabo reiciendis voluptatem porro rerum optio quidem quaerat animi eveniet magnam, pariatur non delectus velit perferendis!</p>
-            <!-- {{ speakersData }} -->
-          </v-col>
-    </v-row>
-    <v-row>
-        <v-col md="2" v-for="(item, index) in speakersData" :key="index">
-            <common-speaker-card :data="item"/>
+          <h1>Speakers</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti,
+            sapiente est architecto autem minus explicabo reiciendis voluptatem
+            porro rerum optio quidem quaerat animi eveniet magnam, pariatur non
+            delectus velit perferendis!
+          </p>
         </v-col>
-    </v-row>
-  </v-container>
-</NuxtLayout>
+      </v-row>
+      <v-row v-if="loading">
+        <v-col md="2" v-for="i in 6" :key="i">
+          <v-skeleton-loader
+            style="border-radius: 12px"
+            type="card"
+          ></v-skeleton-loader>
+        </v-col>
+      </v-row>
+      <v-row v-else>
+        <v-col md="2" v-for="(item, index) in speakersData" :key="index">
+          <common-speaker-card :data="item" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </NuxtLayout>
 </template>
 
 <script setup>
@@ -45,5 +57,4 @@ const getAllSpeakersData = async () => {
 </script>
 
 <style>
-
 </style>

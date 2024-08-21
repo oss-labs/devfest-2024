c0a810256d20916a0643daf94b33f1f2abed52b4
Vrijraj Singh <vrijraj2396@gmail.com>
2024-08-21 20:54:53 +0530

    Update and rename AdminDrawer.vue to AppDrawer.vue

---
+++ components/core/AppDrawer.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-navigation-drawer v-model="sidebar" app floating temporary location="bottom">
+    <v-list-item class="py-4" title="DevFest 2024" :subtitle="configDataSet.communityName"></v-list-item>
+    <v-divider></v-divider>
+    <v-list-item link title="Home" to="/"></v-list-item>
+    <v-list-item link title="Speakers" to="/speakers"></v-list-item>
+    <v-list-item link title="Agenda" to="/agenda"></v-list-item>
+    <v-list-item link title="Badge" to="/badge"></v-list-item>
+    <v-list-item link title="Team" to="/team"></v-list-item>
+    <v-list-item link title="FAQ" to="/faq"></v-list-item>
+  </v-navigation-drawer>
+</template>
+
+<script setup>
+import configData from "/public/data/config.json";
+const sidebar = useSideBar();
+const configDataSet = ref([]);
+configDataSet.value = configData;
+</script>
+
+<style>
+</style>

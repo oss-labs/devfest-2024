0915136a4747493a9a6fe931dc1962488c376b41
vrijraj <vrijraj2396@gmail.com>
2024-08-21 14:58:26 +0530

    data updated

--- pages/team.vue
+++ pages/team.vue
@@ -12,15 +12,49 @@
           </p>
         </v-col>
       </v-row>
+      <v-row v-if="loading">
+        <v-col md="2" v-for="i in 6" :key="i">
+          <v-skeleton-loader
+            style="border-radius: 12px"
+            type="card"
+          ></v-skeleton-loader>
+        </v-col>
+      </v-row>
+      <v-row v-else>
+        <v-col md="2" v-for="(item, index) in teamData" :key="index">
+          <common-speaker-card :data="item" />
+        </v-col>
+      </v-row>
     </v-container>
   </NuxtLayout>
 </template>
-  
+
 <script setup>
+const { getAllTeam } = useTeam();
+
+let loading = ref(true);
+let teamData = ref([]);
+
 definePageMeta({
   layout: false,
 });
+onMounted(() => {
+  getAllTeamData();
+});
+
+const getAllTeamData = async () => {
+  try {
+    loading.value = true;
+    let res = await getAllTeam("team");
+    console.log(res);
+    teamData.value = res;
+    loading.value = false;
+  } catch (error) {
+    console.log("error", error);
+    loading.value = false;
+  }
+};
 </script>
-  
-  <style>
+
+<style>
 </style>

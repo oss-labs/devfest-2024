c172e1743cc626530b75d24c2113f36cc047b37a
sandali-singh <sandalisingh1270@gmail.com>
2024-08-26 17:13:05 +0530

    footer-icon update

--- components/common/speakerSocialButton.vue
+++ components/common/speakerSocialButton.vue
@@ -3,14 +3,14 @@
     <!-- {{ socialLinks }} -->
     <v-btn
       class="mr-1 mb-1"
-      v-if="socialLinks.linkedin!=null && socialLinks.linkedin.length"
+      v-if="socialLinks.linkedin != null && socialLinks.linkedin.length"
       icon="mdi-linkedin"
       size="small"
       variant="flat"
     ></v-btn>
     <v-btn
       class="mr-1 mb-1"
-      v-if="socialLinks.github!=null && socialLinks.github.length"
+      v-if="socialLinks.github != null && socialLinks.github.length"
       icon="mdi-github"
       size="small"
       variant="flat"
@@ -19,13 +19,13 @@
       class="mr-1 mb-1"
       icon="mdi-twitter"
       size="small"
-      v-if="socialLinks.twitter!=null && socialLinks.twitter.length"
+      v-if="socialLinks.twitter != null && socialLinks.twitter.length"
       variant="flat"
     ></v-btn>
     <v-btn
       class="mr-1 mb-1"
       icon="mdi-instagram"
-      v-if="socialLinks.instagram!=null && socialLinks.instagram.length"
+      v-if="socialLinks.instagram != null && socialLinks.instagram.length"
       size="small"
       variant="flat"
     ></v-btn>
@@ -42,5 +42,4 @@ const props = defineProps({
 });
 </script>
 
-<style>
-</style>
+<style></style>
--- components/core/AppFooter.vue
+++ components/core/AppFooter.vue
@@ -1,78 +1,83 @@
 <template>
-  <v-container
-      fluid
-      class="px-5 mt-8"
+  <v-container fluid class="px-5 mt-8" style="background-color: #202124">
+    <v-row
+      justify="center"
+      align="center"
       style="max-width: 1400px; margin-left: auto; margin-right: auto"
     >
-      <v-divider class="mb-4"></v-divider>
-      <v-row justify="center" align="center">
-        <v-col md="2" cols="12">
-          <h3>{{ mainData.communityName }}</h3>
-          <a
-            href="https://github.com/oss-labs/"
-            style="text-decoration: none; color: black"
-            ><p style="font-size: 70%"><i>developed by OSS Labs</i></p></a
-          >
-        </v-col>
-        <v-col md="10" cols="12">
-          <v-container fluid class="pa-0">
-            <v-row>
-              <v-col>
-                <a
-                  href="https://developers.google.com/community/gdg"
-                  target="_blank"
-                  rel="noreferrer"
-                  class="grey--text text--darken-3 mr-6 google-font"
-                  style="text-decoration: none; color: black"
-                  >About GDG Program</a
-                >
-                <br class="hidden-md-and-up" />
-                <a
-                  href="https://www.womentechmakers.com/"
-                  target="_blank"
-                  rel="noreferrer"
-                  class="grey--text text--darken-3 mr-6 google-font"
-                  style="text-decoration: none; color: black"
-                  >About WTM Program</a
-                >
+      <v-col md="2" cols="12">
+        <h2 style="color: white">{{ mainData.communityName }}</h2>
+        <a
+          href="https://github.com/oss-labs/"
+          style="text-decoration: none; color: white"
+          ><p style="font-size: 70%"><i>developed by OSS Labs</i></p></a
+        >
+      </v-col>
+      <v-col md="10" cols="12">
+        <v-container fluid class="pa-0">
+          <v-row justify="" align="center">
+            <v-col md="9" cols="12">
+              <a
+                href="https://developers.google.com/community/gdg"
+                target="_blank"
+                rel="noreferrer"
+                class="grey--text text--darken-3 mr-6 google-font"
+                style="text-decoration: none; color: white"
+                >About GDG Program</a
+              >
+              <br class="hidden-md-and-up" />
+              <a
+                href="https://www.womentechmakers.com/"
+                target="_blank"
+                rel="noreferrer"
+                class="grey--text text--darken-3 mr-6 google-font"
+                style="text-decoration: none; color: white"
+                >About WTM Program</a
+              >
 
-                <br class="hidden-md-and-up" />
-                <router-link
-                  to="/faq"
-                  rel="noreferrer"
-                  class="mr-3 google-font grey--text text--darken-3"
-                  style="text-decoration: none; color: black"
-                  >FAQ</router-link
-                >
-                <br class="hidden-md-and-up" />
-                <router-link
-                  to="/coc"
-                  style="text-decoration: none; color: black"
-                  rel="noreferrer"
-                  class="mr-3 google-font grey--text text--darken-3"
-                  >Code of conduct</router-link
-                >
-                <br class="hidden-md-and-up" />
-                <a
-                  href="https://developers.google.com/community-guidelines"
-                  target="_blank"
-                  rel="noreferrer"
-                  class="mr-6 google-font grey--text text--darken-3"
-                  style="text-decoration: none; color: black"
-                  >Community Guidelines</a
-                >
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-col>
-      </v-row>
-    </v-container>
+              <br class="hidden-md-and-up" />
+              <router-link
+                to="/faq"
+                rel="noreferrer"
+                class="mr-6 google-font grey--text text--darken-3"
+                style="text-decoration: none; color: white"
+                >FAQ</router-link
+              >
+              <br class="hidden-md-and-up" />
+              <router-link
+                to="/coc"
+                style="text-decoration: none; color: white"
+                rel="noreferrer"
+                class="mr-6 google-font grey--text text--darken-3"
+                >Code of conduct</router-link
+              >
+              <br class="hidden-md-and-up" />
+              <a
+                href="https://developers.google.com/community-guidelines"
+                target="_blank"
+                rel="noreferrer"
+                class="mr-6 google-font grey--text text--darken-3"
+                style="text-decoration: none; color: white"
+                >Community Guidelines</a
+              >
+            </v-col>
+
+            <v-col md="3" cols="12">
+              <CommonSpeakerSocialButton
+                :socialLinks="mainData.communityLinks"
+                :dark="true"
+                class="footer-icons"
+              />
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
 const { mainData } = useJSONData();
 </script>
 
-<style>
-
-</style>
+<style></style>
